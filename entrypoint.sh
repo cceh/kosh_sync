b7993e6e2ac2a5e31b4c1a14d15cee897138006f
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2022-09-22 15:18:44 +0200

    Add initialization routine

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,16 @@ if test -z "$KOSH_SYNC_ORIGIN"; then
   exit 1
 fi
 
+if test ! -d "$KOSH_SYNC_FOLDER/.git"; then
+  echo -n "$(date +"%Y-%m-%d %H:%M:%S") [INIT] "
+  git clone -b "$KOSH_SYNC_BRANCH" "$KOSH_SYNC_ORIGIN" "$KOSH_SYNC_FOLDER" \
+     2>&1 | head -n1
+fi
+
 while true; do
   echo -n "$(date +"%Y-%m-%d %H:%M:%S") [SYNC] "
-  git -C "$KOSH_SYNC_FOLDER" pull "$KOSH_SYNC_ORIGIN" "$KOSH_SYNC_BRANCH" 2>&1 \
-    | tail -n+3
+  git -C "$KOSH_SYNC_FOLDER" pull "$KOSH_SYNC_ORIGIN" "$KOSH_SYNC_BRANCH" \
+    2>&1 | tail -n+3
 
   sleep $KOSH_SYNC_REPOSE
 done
